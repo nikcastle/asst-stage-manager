fda3e050f2ce9a46ef0600134ca07e15eb553ec9
Lady Stock <nikki.castlea@gmail.com>
2020-09-16 15:56:20 -0600

    api routes for users and notes

---
+++ client/src/utils/API.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export default {
+   
+    //Internal API database calls
+    getUser: currentUser => {
+        return axios.get(`/api/users/${currentUser}`)
+    },
+
+    createUser: userData => {
+        return axios.post(`/api/users`, userData)
+    },
+
+    populateList: id => {
+        return axios.get(`/api/users/populate/${id}`)
+    },
+
+    findUsers: () => {
+        return axios.get(`/api/users`)
+    },
+
+    addNote: (id, currentUser) => {
+        return axios.get(`/api/notes/item/${id}?currentUser=${currentUser}`)
+    },
+
+    removeNote: (id, currentUser) => {
+        return axios.get(`/api/notes/remove/${id}?currentUser=${currentUser}`)
+    }
+
+}
